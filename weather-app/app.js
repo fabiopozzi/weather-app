f4f7988c0b6ebf617f0fc96388e3d31be476ea30
Fabio Pozzi <fabio@fabiopozzi.net>
2020-03-14 15:55:12 +0100

    get JSON data via darksky API and parse the resulting object. Data extraction challenge completed.

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,7 +2,8 @@ const request = require('request')
 
 const url = 'https://api.darksky.net/forecast/af375ff38c946c66aec4e47aa88fe561/45.4501,8.6167'
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.currently)
+request({ url: url, json: true }, (error, response) => {
+    const temp = response.body.currently.temperature
+    const rain_chance = response.body.currently.precipProbability
+    console.log(`It is currently ${temp} degrees out. There is a ${rain_chance}% chance of rain.`)
 })
