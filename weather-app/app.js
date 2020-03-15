9cfd8b955d7108c8b501b0e66798c45dd0c3db31
Fabio Pozzi <fabio@fabiopozzi.net>
2020-03-15 16:28:24 +0100

    Customize HTTP request. Use more infos taken from response to improve forecast. Traslate to italian.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,10 @@
 const request = require('request')
 
-const url = 'https://api.darksky.net/forecast/af375ff38c946c66aec4e47aa88fe561/45.4501,8.6167'
+const url = 'https://api.darksky.net/forecast/af375ff38c946c66aec4e47aa88fe561/45.4501,8.6167?units=si&lang=it'
 
 request({ url: url, json: true }, (error, response) => {
     const temp = response.body.currently.temperature
     const rain_chance = response.body.currently.precipProbability
-    console.log(`It is currently ${temp} degrees out. There is a ${rain_chance}% chance of rain.`)
+    console.log(response.body.daily.data[0].summary)
+    console.log(`Al momento ci sono ${temp}°C fuori. Le probabilità di pioggia sono ${rain_chance}%.`)
 })
