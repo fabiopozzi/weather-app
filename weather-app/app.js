f5638539db784565e955ed35f3a2cbc1bb5389a4
Fabio Pozzi <fabio@fabiopozzi.net>
2020-03-17 23:18:08 +0100

    Refactor to use ES6 destructuring syntax.

--- weather-app/app.js
+++ weather-app/app.js
@@ -5,17 +5,17 @@ if (process.argv.length < 3) {
     return console.log('Missing argument')
 }
 
-geocode(process.argv[2], (error, data) => {
+geocode(process.argv[2], (error, { latitude, longitude, location }) => {
     if (error) {
         return console.log(error)
     }
 
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
+    forecast(latitude, longitude, (error, forecastData) => {
         if (error) {
             return console.log(error)
         }
 
-        console.log(data.location)
+        console.log(location)
         console.log(forecastData)
     })
 })
